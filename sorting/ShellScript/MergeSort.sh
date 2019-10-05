76014be3f75f2ccbe1cc847a4724f77a03d94b83
Kanval Kalra <kanval87@gmail.com>
2019-10-05 19:50:27 +0530

    Removed : array forwarding in recursion -> seems there are some finer details to be taken care of in case of forwarding array in shell script

--- sorting/ShellScript/MergeSort.sh
+++ sorting/ShellScript/MergeSort.sh
@@ -4,30 +4,29 @@ merge(){
     local p=$1
     local q=$2
     local r=$3
-    local array=(${@:4})
-    echo "merge -> ${array[@]} 
-    $p $q $r"
+    echo "merge  p -> $p : q -> $q : r -> $r "
 }
 
 
 mergeSort(){
     local p=$1
     local r=$2
-    local localArray=(${@:3})
     local diff=$(expr $r - $p)
-    echo $diff
-    if [ $diff -gt 0 ]
+    if [ $diff -gt 1 ]
     then
         q=$(expr $(expr $r + $p) / 2)
-        mergeSort $p $q $localArray
-        mergeSort $(expr $q + 1) $r $localArray
-        merge $p $q $r $localArray
+        qNext=$(expr $q  + 1)
+        echo "mergesort -> p-> $p : q -> $q : qNext -> $qNext  : r -> $r | ${inputArray[$r]} : array -> ${inputArray[@]}"
+        mergeSort $p $q
+        mergeSort $qNext $r
+        merge $p $q $r
     else
+        echo "return mergesort -> p-> $p  : r -> $r : array -> ${inputArray[@]}"
         return
     fi    
 }
 
 
 inputArray=(1 2 3 4 5 6)
-mergeSort  0 ${#inputArray[@]} ${inputArray[@]}
+mergeSort  0 $(expr ${#inputArray[@]} - 1)
 
