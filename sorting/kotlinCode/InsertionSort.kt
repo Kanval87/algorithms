8a0539a0d15eba7e5a3c63a206311945a2455332
Kanval Kalra <kanval87@gmail.com>
2019-10-18 16:00:55 +0530

    - starting with making copy of earlier done code in kotlin and then onward kotlin only until master it adequetly

---
+++ sorting/kotlinCode/InsertionSort.kt
@@ -0,0 +1,10 @@
+package kotlinCode
+
+import kotlin.text.*
+import java.util.Arrays
+
+var inputArray = arrayOf(1, 2, 3, 4, 5, 6)
+
+fun main() {
+    println("Insertion sort -> " + Arrays.toString(inputArray))
+}
