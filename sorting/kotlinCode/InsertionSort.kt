cde3926fc0c95c37ca5256070d55e6dc74c8ba31
Kanval Kalra <kanval87@gmail.com>
2019-10-18 17:08:00 +0530

    - Wrote/Copied insertion sort code from shell script code to kotlin

--- sorting/kotlinCode/InsertionSort.kt
+++ sorting/kotlinCode/InsertionSort.kt
@@ -3,8 +3,27 @@ package kotlinCode
 import kotlin.text.*
 import java.util.Arrays
 
-var inputArray = arrayOf(1, 2, 3, 4, 5, 6)
-
 fun main() {
-    println("Insertion sort -> " + Arrays.toString(inputArray))
+    var inputArray = arrayOf(1, 2, 3, 4, 5, 6)
+    println("Insertion sort input array -> " + Arrays.toString(inputArray))
+    var sortedArray = insertionSort(inputArray)
+    println("sorted input array -> " + Arrays.toString(sortedArray))
+}
+
+fun insertionSort(array : Array<Int>): Array<Int> {
+    for(index in array.indices){
+        when(index){
+            0 -> print("Skipping\n")
+            else -> {
+                var key = array[index]
+                var i = index - 1
+                while(i > -1 && key > array[i]){
+                    array[i + 1] = array[i]
+                    i = i - 1
+                }
+                array[i + 1] = key
+            }
+        }
+    }
+    return array
 }
