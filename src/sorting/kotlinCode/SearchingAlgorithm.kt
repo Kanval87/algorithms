62e7d654b98ee988aa3a0adfde89182737b41821
Kanval Kalra <kanval87@gmail.com>
2019-10-28 05:13:08 +0530

    preparing for new algorithm test

--- src/sorting/kotlinCode/SearchingAlgorithm.kt
+++ src/sorting/kotlinCode/SearchingAlgorithm.kt
@@ -8,26 +8,30 @@ class SearchingAlgorithm {
         var numToSearch: Int = /*Random().nextInt(7)*/ 2
         var arrayToSearchIn = sortingCode.getSortedArray().clone()
 
-//        for ((index, value) in arrayToSearchIn.withIndex()) {
-//            var arrayToSearchIn = sortingCode.getSortedArray().clone()
+        for ((index, value) in arrayToSearchIn.withIndex()) {
+            var arrayToSearchIn = sortingCode.getSortedArray().clone()
+            var result = binarySearchWithIndex(value, arrayToSearchIn, 0, false)
 //            var result = binarySearch(value, arrayToSearchIn, 0, false)
-//            if (!result.hasFoundNum) {
-//                println("could not find $value in ${arrayToSearchIn.contentToString()}")
-//            } else {
-//                println("found $value at ${result.numberFoundAt} in ${arrayToSearchIn.contentToString()}")
-//            }
-//        }
-
-        var result = binarySearch(numToSearch, arrayToSearchIn, 0, false)
-        if (!result.hasFoundNum) {
-            println("could not find $numToSearch in ${arrayToSearchIn.contentToString()}")
-        } else {
-            println("found $numToSearch at ${result.numberFoundAt} in ${arrayToSearchIn.contentToString()}")
+            if (!result.hasFoundNum) {
+                println("could not find $value in ${arrayToSearchIn.contentToString()}")
+            } else {
+                println("found $value at ${result.numberFoundAt} in ${arrayToSearchIn.contentToString()}")
+            }
         }
+
+//        var result = binarySearch(numToSearch, arrayToSearchIn, 0, false)
+//        if (!result.hasFoundNum) {
+//            println("could not find $numToSearch in ${arrayToSearchIn.contentToString()}")
+//        } else {
+//            println("found $numToSearch at ${result.numberFoundAt} in ${arrayToSearchIn.contentToString()}")
+//        }
     }
 
+    private fun binarySearch() : ResultAndIndex{
+        return ResultAndIndex( true , 0)
+    }
 
-    private fun binarySearch(numToSearch: Int, sortedArray: IntArray, midNumberParam: Int, isFromRight: Boolean): ResultAndIndex {
+    private fun binarySearchWithIndex(numToSearch: Int, sortedArray: IntArray, midNumberParam: Int, isFromRight: Boolean): ResultAndIndex {
         println("Checking $numToSearch with indexTrack $midNumberParam in ${sortedArray.contentToString()} coming from Right -> $isFromRight")
         if (sortedArray.isNotEmpty()) {
             var midIndex: Int
@@ -37,14 +41,15 @@ class SearchingAlgorithm {
                     when {
                         sortedArray[midIndex] <= numToSearch -> {
                             var slicedArray = sortedArray.sliceArray(0 until midIndex + 1)
-                            return binarySearch(numToSearch, slicedArray, midNumberParam, false)
+//                            return binarySearch(numToSearch, slicedArray, midNumberParam, false)
+                            return binarySearchWithIndex(numToSearch, slicedArray, midNumberParam, false)
                         }
                         sortedArray[midIndex + 1] >= numToSearch -> {
                             var slicedArray = sortedArray.sliceArray(midIndex + 1 until sortedArray.size)
                             if (isFromRight)
-                                return binarySearch(numToSearch, slicedArray, midNumberParam + midIndex, true)
+                                return binarySearchWithIndex(numToSearch, slicedArray, midNumberParam + midIndex, true)
                             else
-                                return binarySearch(numToSearch, slicedArray, midNumberParam + midIndex + 1, true)
+                                return binarySearchWithIndex(numToSearch, slicedArray, midNumberParam + midIndex + 1, true)
                         }
                     }
                 } else {
@@ -65,13 +70,15 @@ class SearchingAlgorithm {
                 midIndex = sortedArray.size / 2
                 when {
                     sortedArray[midIndex] == numToSearch -> return ResultAndIndex(true, midNumberParam + midIndex)
-                    sortedArray[midIndex] <= numToSearch -> return binarySearch(numToSearch, sortedArray.sliceArray(0 until midIndex), midNumberParam, false)
-                    sortedArray[midIndex] >= numToSearch -> return binarySearch(numToSearch, sortedArray.sliceArray(midIndex + 1 until sortedArray.size), midNumberParam + midIndex, true)
+                    sortedArray[midIndex] <= numToSearch -> return binarySearchWithIndex(numToSearch, sortedArray.sliceArray(0 until midIndex), midNumberParam, false)
+                    sortedArray[midIndex] >= numToSearch -> return binarySearchWithIndex(numToSearch, sortedArray.sliceArray(midIndex + 1 until sortedArray.size), midNumberParam + midIndex, true)
                 }
             }
         }
         return ResultAndIndex(false, 0)
     }
+
+
 
     /*  Supporting classes  */
     class ResultAndIndex {
