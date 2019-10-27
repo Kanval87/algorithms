b88b0d8d43abb6d4862dd5172ab7d102934aebda
Kanval Kalra <kanval87@gmail.com>
2019-10-27 15:31:48 +0530

    Almost done -- one small error remains

--- src/sorting/kotlinCode/SearchingAlgorithm.kt
+++ src/sorting/kotlinCode/SearchingAlgorithm.kt
@@ -5,8 +5,9 @@ import java.util.*
 class SearchingAlgorithm {
     private var sortingCode = SortingCode()
     fun run() {
-        var numToSearch: Int = /*Random().nextInt(7)*/ 4
+//        var numToSearch: Int = /*Random().nextInt(7)*/ 2
         var arrayToSearchIn = sortingCode.getSortedArray().clone()
+
         for ((index, value) in arrayToSearchIn.withIndex()) {
             var arrayToSearchIn = sortingCode.getSortedArray().clone()
             var result = binarySearch(value, arrayToSearchIn, 0, false)
@@ -16,6 +17,7 @@ class SearchingAlgorithm {
                 println("found $value at ${result.numberFoundAt} in ${arrayToSearchIn.contentToString()}")
             }
         }
+
 //        var result = binarySearch(numToSearch, arrayToSearchIn, 0, false)
 //        if (!result.hasFoundNum) {
 //            println("could not find $numToSearch in ${arrayToSearchIn.contentToString()}")
@@ -27,17 +29,32 @@ class SearchingAlgorithm {
     private fun binarySearch(numToSearch: Int, sortedArray: IntArray, midNumberParam: Int, isFromRight: Boolean): ResultAndIndex {
         println("Checking $numToSearch with indexTrack $midNumberParam in ${sortedArray.contentToString()} coming from Right -> $isFromRight")
         if (sortedArray.isNotEmpty()) {
-            val midIndex: Int
+            var midIndex: Int
             if (sortedArray.size % 2 == 0) {
                 midIndex = (sortedArray.size / 2) - 1
-                when {
-                    sortedArray[midIndex] <= numToSearch -> {
-                        var slicedArray = sortedArray.sliceArray(0 until midIndex + 1)
-                        return binarySearch(numToSearch, slicedArray, midNumberParam, false)
+                if (sortedArray.size > 2) {
+                    when {
+                        sortedArray[midIndex] <= numToSearch -> {
+                            var slicedArray = sortedArray.sliceArray(0 until midIndex + 1)
+                            return binarySearch(numToSearch, slicedArray, midNumberParam, false)
+                        }
+                        sortedArray[midIndex + 1] >= numToSearch -> {
+                            var slicedArray = sortedArray.sliceArray(midIndex + 1 until sortedArray.size)
+                            return binarySearch(numToSearch, slicedArray, midNumberParam + midIndex, true)
+                        }
                     }
-                    sortedArray[midIndex + 1] >= numToSearch -> {
-                        var slicedArray = sortedArray.sliceArray(midIndex + 1 until sortedArray.size)
-                        return binarySearch(numToSearch, slicedArray, midNumberParam + midIndex, true)
+                } else {
+                    var valueToAdd = 0
+                    if (isFromRight) {
+                        valueToAdd = 1
+                    }
+                    when {
+                        sortedArray[midIndex] == numToSearch -> {
+                            return ResultAndIndex(true, midNumberParam + midIndex + valueToAdd)
+                        }
+                        sortedArray[midIndex + 1] == numToSearch -> {
+                            return ResultAndIndex(true, midNumberParam + midIndex + valueToAdd + 1)
+                        }
                     }
                 }
             } else {
